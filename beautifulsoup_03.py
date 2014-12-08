16c1ad795c40762ba6a5d0dd135b7d68b3a320a5
sarah1985 <sarahmleavy@gmail.com>
2014-12-08 12:50:36 -0500

    task 3

--- beautifulsoup_03.py
+++ beautifulsoup_03.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 03: Lesson 15"""
+
 import urllib2
 from bs4 import BeautifulSoup
 
@@ -5,9 +9,29 @@ URL = 'http://sps.cuny.edu/whoweare/departments/it'
 DATA = urllib2.urlopen(URL).read()
 SOUP = BeautifulSoup(DATA)
 
-# Put your function here
+
+def sps_it_department():
+    """sps it contacts"""
+
+    soup_names = SOUP.html.find_all("span", {"class": "name bold"})
+    emails = SOUP.select("a[href^=mailto]")
+
+    contacts = []
+    for index in range(len(soup_names)):
+        full_name = soup_names[index].get_text().split(',')
+        first_name = full_name[1].strip()
+        last_name = full_name[0].strip()
+        email = emails[index].get_text()
+
+        contact_dict = {
+            "First Name": first_name,
+            "Last Name": last_name,
+            "Email": email
+        }
+
+        contacts.append(contact_dict)
+
+    return contacts
 
 if __name__ == '__main__':
-    # You can use this conditional block for debugging
-    # print SOUP
-    pass
+    print sps_it_department()
